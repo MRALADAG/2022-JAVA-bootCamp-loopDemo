e05184030fb7d3ab195b9bd012f6d00c7f5c914e
MRALADAG <mstf_aladag@yahoo.com>
2022-09-17 14:03:28 +0300

    loop exercises

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("\nEven numbers.");
+
+        for (int i = 2; i < 10; i += 2) {
+            System.out.println(i);
+        }
+
+        System.out.println("\nOdd numbers.");
+
+        for (int i = 1; i < 10; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
